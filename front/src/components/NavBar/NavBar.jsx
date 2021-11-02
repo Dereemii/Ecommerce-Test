48802c300d953abcb3b0afdded95cc36b0ae82dd
Leslie Herrera <lherrera@ecomsur.com>
2021-11-02 18:59:30 -0300

    adding font aweseome library

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import './NavBar.css'
+
 const NavBar = () => {
     return (
         <>
             <nav>
-                Navbar
+                <ul>
+                    <div>
+                        <li>Logo</li>
+                        <li>About Us</li>
+                        <li>Out Products</li>
+                    </div>
+
+                    <div>
+                        <li><FontAwesomeIcon icon={faShoppingBag} /></li>
+                    </div>
+                </ul>
             </nav>
         </>
     );
